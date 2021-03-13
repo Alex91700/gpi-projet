254fe4572357f08a33a0f5f4612781ce06cf59aa
Ryan GHOMARI <ryaneghomari@gmail.com>
2021-03-13 18:47:00 +0100

    commandes

--- Commandes/admin.py
+++ Commandes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Commandes, Cheques, CB
 # Register your models here.
+
+aCommandes = [Commandes, Cheques, CB]
+admin.site.register(aCommandes)
--- Commandes/models.py
+++ Commandes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Commandes(models.Model):
+    individu = models.ManyToManyField(Individu)
+    quantité = models.Integer(max_digits = 5)
+    ref_article = models.Integer(max_digits = 15)
+    type_reglement = models.ForeignKey(Reglement, on_delete=models.CASCADE)
+
+class Reglement(models.Model):
+    cb = models.ForeignKey(CB, on_delete=models.CASCADE)
+    cheque = models.ForeignKey(Cheques, on_delete=models.CASCADE)
+
+class Cheques(models.Model):
+    num_cheque = models.Integer(max_digits=10)
+    nom_banque = models.CharField(max_length=100)
+
+class CB(models.Model):
+    num_carte = models.IntegerField(max_digits=10)
+    date_expiration = models.DateField()
